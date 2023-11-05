89c2c266d6cbe4e20ead056acde5c76ce26ff3c6
dani <dani@dani.com>
2023-11-05 19:11:31 +0100

    Added responsive and active states

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-const primaryNav = document.querySelector('.primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
+const links = document.querySelectorAll('.mobile-nav a');
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+const hero_btn = document.querySelector('.nav-container a');
 
-navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute('data-visible');
+window.onload = function () {
+	window.addEventListener('scroll', function (e) {
+		if (window.pageYOffset > 100) {
+			document.querySelector("header").classList.add('is-scrolling');
+		} else {
+			document.querySelector("header").classList.remove('is-scrolling');
+		}
+	});
 
-    if (visibility === 'false') {
-        primaryNav.setAttribute('data-visible', true);
-        navToggle.setAttribute('aria-expanded', true);
-    }else if (visibility === 'true') {
-        primaryNav.setAttribute('data-visible', false);
-        navToggle.setAttribute('aria-expanded', false);
-    }
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+
+	hero_btn.addEventListener('click', function() {
+		mobile_menu.classList.remove('is-active');
+		menu_btn.classList.remove('is-active');
+	})
+}
+
+links.forEach((element) => {
+    element.addEventListener('click', () => {
+		mobile_menu.classList.remove('is-active');
+		menu_btn.classList.toggle('is-active');
+    })
 })
